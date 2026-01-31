10f23d4d1dd377b8441343ab665fb9deda85dc93
spastorsegura <67179586+spastorsegura@users.noreply.github.com>
2026-01-31 10:13:25 -0500

    feat: add frontend URL to CORS configuration

--- backend/config/cors.ts
+++ backend/config/cors.ts
@@ -1,5 +1,5 @@
 export default {
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://zipo-self.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
